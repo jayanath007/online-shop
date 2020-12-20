3f3c61448b25ff80f09529d8482d3305af918260
lushan <LJayanath@dpssoftware.co.uk>
2020-12-20 20:07:21 +0530

    update

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentComponent } from './content/content.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './content/footer/footer.component';
 import { AppRoutingModule } from '../app-routing.module';
 
 
