b64b71b0e080d7b31d6ffdab5212e9779011919c
lushan <LJayanath@dpssoftware.co.uk>
2020-12-23 22:47:32 +0530

    update

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,11 +1,13 @@
 import { AgmCoreModule } from '@agm/core';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MapComponent } from './map/map.component';
 import { ProductsHomeComponent } from './products-home/products-home.component';
 import { ProductsListItemComponent } from './products-list-item/products-list-item.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { SearchComponent } from './search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -14,7 +16,9 @@ import { SearchComponent } from './search/search.component';
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAz5DChTHUHSGcHR3BOLvjDFfLTfZQ1E8I',
       libraries: ['places']
-    })
+    }),
+    BrowserAnimationsModule,
+    NgbModule,
   ],
   exports: [
     ProductsListComponent,
