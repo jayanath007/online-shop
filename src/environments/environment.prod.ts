e292cfbc16241d51edc42033d27edd73b58a43c2
lushan <LJayanath@dpssoftware.co.uk>
2020-12-28 22:32:26 +0530

    integrate with fire base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAz5DChTHUHSGcHR3BOLvjDFfLTfZQ1E8I",
+    authDomain: "online-shop-299505.firebaseapp.com",
+    databaseURL: "https://online-shop-299505-default-rtdb.firebaseio.com",
+    projectId: "online-shop-299505",
+    storageBucket: "online-shop-299505.appspot.com",
+    messagingSenderId: "952363420988",
+    appId: "1:952363420988:web:a85dbfa5020fa729fc6239",
+    measurementId: "G-YEBDLLDVV9"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,19 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    // apiKey: "AIzaSyAz5DChTHUHSGcHR3BOLvjDFfLTfZQ1E8I",
-    // authDomain: "online-shop-299505.firebaseapp.com",
-    // projectId: "online-shop-299505",
-    // storageBucket: "online-shop-299505.appspot.com",
-    // messagingSenderId: "952363420988",
-    // appId: "1:952363420988:web:35e579f7264fdc5efc6239",
-    // measurementId: "G-B727J7CQXD"
-    apiKey: 'AIzaSyCXVTjTuBUXf-UWlpKfKHjomfPezutmPwI',
-    authDomain: 'cas-fee-shop.firebaseapp.com',
-    databaseURL: 'https://cas-fee-shop.firebaseio.com',
-    projectId: 'cas-fee-shop',
-    storageBucket: 'cas-fee-shop.appspot.com',
-    messagingSenderId: '323643286137'
+    apiKey: "AIzaSyAz5DChTHUHSGcHR3BOLvjDFfLTfZQ1E8I",
+    authDomain: "online-shop-299505.firebaseapp.com",
+    databaseURL: "https://online-shop-299505-default-rtdb.firebaseio.com",
+    projectId: "online-shop-299505",
+    storageBucket: "online-shop-299505.appspot.com",
+    messagingSenderId: "952363420988",
+    appId: "1:952363420988:web:a85dbfa5020fa729fc6239",
+    measurementId: "G-YEBDLLDVV9"
   }
 };
 
