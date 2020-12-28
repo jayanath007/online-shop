56e6cf88ef5e492a87fe0ad36a81e62acd8d1d04
lushan <LJayanath@dpssoftware.co.uk>
2020-12-28 12:29:36 +0530

    fire base add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ProductsModule } from './products/products.module';
 import { OffcanvasService } from './core/offcanvas.service';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
 
 @NgModule({
   declarations: [
@@ -16,6 +20,9 @@ import { OffcanvasService } from './core/offcanvas.service';
     AppRoutingModule,
     CoreModule,
     ProductsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
   ],
   providers: [OffcanvasService],
   bootstrap: [AppComponent]
